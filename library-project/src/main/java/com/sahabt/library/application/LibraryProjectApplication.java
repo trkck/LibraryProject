e508394083502efd2b0bd86500ae012c96df5616
Ugur <77512251+turna-94@users.noreply.github.com>
2022-03-23 15:44:06 +0300

    ugur deneme

--- library-project/src/main/java/com/sahabt/library/application/LibraryProjectApplication.java
+++ library-project/src/main/java/com/sahabt/library/application/LibraryProjectApplication.java
@@ -8,6 +8,7 @@ public class LibraryProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryProjectApplication.class, args);
+		System.out.println("Ugur deneme");
 		
 	}
 
