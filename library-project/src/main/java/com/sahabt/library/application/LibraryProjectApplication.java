e5f7e27709a26c77e0859a19e79a807bc4a87f0e
Ugur <77512251+turna-94@users.noreply.github.com>
2022-03-23 15:50:47 +0300

    Update LibraryProjectApplication.java

--- library-project/src/main/java/com/sahabt/library/application/LibraryProjectApplication.java
+++ library-project/src/main/java/com/sahabt/library/application/LibraryProjectApplication.java
@@ -9,6 +9,7 @@ public class LibraryProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryProjectApplication.class, args);
 		System.out.println("Ugur deneme");
+		System.out.println("Deneme 2");
 		
 	}
 
